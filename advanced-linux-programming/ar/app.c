8b794bdb65da0db7c974a0112d683af92ffb085c
cgwu <gg.wcg@qq.com>
2015-01-15 12:03:26 +0800

    shared library test

--- advanced-linux-programming/ar/app.c
+++ advanced-linux-programming/ar/app.c
@@ -7,7 +7,17 @@ int main(int argc, char *argv[])
 }
 /*
 shell command:
+
+static library compile:
 gcc -c test.c
 ar cr libtest.a test.o
-gcc -o app app.c -L. -ltest
+gcc -o app.out app.c -L. -ltest
+
+shared (dynamic linking) library compile:
+gcc -c test.c
+gcc -shared -fPIC libtest.so test.o
+gcc -o app_shared.out -L. -ltest
+导出动态库环境变量:
+LD_LIBRARY_PATH=./
+export LD_LIBRARY_PATH
 */
