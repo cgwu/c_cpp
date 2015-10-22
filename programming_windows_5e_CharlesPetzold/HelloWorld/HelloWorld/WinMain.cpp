4bf723042443b06e0ad27963c7f61141334fe92b
cgwu <gg.wcg@qq.com>
2015-10-22 23:20:33 +0800

    programming_windows_5e_CharlesPetzold 第一个程序。

---
+++ programming_windows_5e_CharlesPetzold/HelloWorld/HelloWorld/WinMain.cpp
@@ -0,0 +1,8 @@
+#include <Windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+				PSTR szCmdLine, int iCmdShow)
+{
+	MessageBox(NULL, TEXT("HelloÖÐ¹ú!"),TEXT("HelloMsg"),0);
+	return 0;
+}
