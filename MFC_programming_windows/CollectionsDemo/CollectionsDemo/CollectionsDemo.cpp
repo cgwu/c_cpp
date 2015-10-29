ee8741c9e3473d95f93b74762f8a8768813502c3
cgwu <gg.wcg@qq.com>
2015-10-29 18:11:08 +0800

    MFC Collection done.

--- MFC_programming_windows/CollectionsDemo/CollectionsDemo/CollectionsDemo.cpp
+++ MFC_programming_windows/CollectionsDemo/CollectionsDemo/CollectionsDemo.cpp
@@ -15,6 +15,13 @@ CWinApp theApp;
 
 using namespace std;
 
+UINT AFXAPI HashKey(CString& string){
+	LPCTSTR key = (LPCTSTR)string;
+	UINT nHash = 0;
+	while(*key) nHash = (nHash << 5) + nHash + *key ++;
+	return nHash;
+}
+
 void TestCollections(){
 	_tsetlocale (LC_ALL, _T("chs"));		//解决中文乱码的问题
 	// CArray可增长的数组,与 vector相似
@@ -58,7 +65,50 @@ void TestCollections(){
 		CString str = sl.GetNext(pos);
 		_tprintf(_T("%ls\n"), str.GetBuffer() );
 	}
+	//CList 范型
+	CList<CPoint, CPoint&> list;
+	//第一个模板参数指定了数据类型(CPoint对象)
+	//第二个参数指出参数列表中列表项的传送方式(通过引用)
+	//CList中包含自定义类型时，需要重载==运算符
+	for (int i = 0; i < 10; i++)
+	{
+		list.AddTail(CPoint(i * 10, i));
+	}
+	POSITION posList = list.GetHeadPosition();
+	while(posList != NULL){
+		CPoint pt = list.GetNext(posList);
+		TRACE(_T("x=%d, y=%d\n"),pt.x, pt.y);
+	}
+	// MFC映射表类型
+	CMapStringToString map;
+	map[_T("Sun")] = _T("星期天");
+	map[_T("Mon")] = _T("星期一");
+	map[_T("Tue")] = _T("星期二");
+	map[_T("Wed")] = _T("星期三");
+	map[_T("Thu")] = _T("星期四");
+	map[_T("Fri")] = _T("星期五");
+	map[_T("Sat")] = _T("星期六");
+	CString strDay;
+	if(map.Lookup(_T("Fri"), strDay)){
+		TRACE(_T("你要找的天为:%s\n"), strDay);
+	}
+	POSITION posMap = map.GetStartPosition();
+	while(posMap != NULL){
+		CString strKey, strItem;
+		map.GetNextAssoc(posMap, strKey, strItem);
+		TRACE(_T("键= %s, 值= %s\n"), strKey, strItem);
+	}
+	//任意类型的CMap
+	CMap<CString, LPCTSTR, CPoint,CPoint&> mapSP;
+	mapSP[CString(_T("p1"))] = CPoint(1,2);
+	mapSP[CString(_T("p2"))] = CPoint(11,22);
+	CPoint p2;
+	if(mapSP.Lookup(_T("p2"),p2)){
+		TRACE(_T("p2 = (%d, %d)"), p2.x, p2.y);
+	}
 }
+
+
 
 int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 {
